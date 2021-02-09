438870c6f721ec8d9b5bf8f92fb376410f959a5f
Randal Schneider <schneider.randal@gmail.com>
2021-02-08 18:00:02 -0600

    changed proxy server to match new ip addresses

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
 require('newrelic');
 const express = require('express');
+const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
 
 const app = express();
-app.use(cors());
 
+app.use(morgan('dev'));
+app.use(cors());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-const PORT = 3001;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log('Server is listening on: ', PORT);
